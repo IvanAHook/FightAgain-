a20129c4c24586db5280cc4cc761011f5abaf18b
unknown <ivan.hook@WH9314BC.CM.lan>
2015-10-09 15:59:44 +0200

    enemy spawning improved but needs more work

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,6 +7,7 @@ public class EnemyManager : MonoBehaviour {
 	FileReader _fReader;
 
 	List<string> enemyWaves;
+	List<int> tempRange;
 	int wave;
 	bool spawning;
 	List<Enemy> enemies = new List<Enemy>();
@@ -21,16 +22,13 @@ public class EnemyManager : MonoBehaviour {
 
 	void Awake() 
 	{
-		List<int> tempRange = new List<int>();
+		tempRange = new List<int>();
 		tempRange.Add( 1 );
 		tempRange.Add( 2 );
-		tempRange.Add( 3 );
-		List<int> tempWave = GenerateWave( 10, tempRange );
-		foreach( int i in tempWave ) {
-			//Debug.Log( i );
-		}
+		//tempRange.Add( 3 );
+		//List<int> tempWave = GenerateWave( 10, tempRange );
 
-		TextAsset wavesFile = Resources.Load( "waves" ) as TextAsset;
+		/*TextAsset wavesFile = Resources.Load( "waves" ) as TextAsset;
 		List<string> fileLines = new List<string>();
 		string [] linesFromFile = wavesFile.text.Split( "\n"[0] );
 		
@@ -38,13 +36,13 @@ public class EnemyManager : MonoBehaviour {
 			if( line != null )
 				fileLines.Add( line );
 		}
-		enemyWaves = fileLines;
+		enemyWaves = fileLines;*/
 
 		//_fReader = new FileReader();
 		//enemyWaves = _fReader.ReadWavesFile( "waves" );
 		wave = 0;
 		if ( spawnOne )
-			enemies.Add(SpawnEnemy(1));
+			enemies.Add( SpawnEnemy( 1 ) );
 	}
     
     void Update() 
@@ -64,54 +62,62 @@ public class EnemyManager : MonoBehaviour {
 			}
 
 		} 
-		else if( wave < enemyWaves.Count  && spawning == false && !spawnOne ) 
+		else if(/* wave < enemyWaves.Count  && */spawning == false && !spawnOne ) 
 		{
-			StartCoroutine( SpawnWave() );
+			Debug.Log(1);
+			List<int> tempList = GenerateWave( (wave+1)*8, tempRange );
+			for ( int i = 0; i < tempList.Count; i++ ) {
+				Debug.Log( tempList[ i ] );
+			}
+			StartCoroutine( SpawnWave( tempList ) );
 		}
 
     }
 
 	List<int> GenerateWave( int resource, List<int> range )
 	{
-		List<int> wave = new List<int>();
+		Debug.Log(2);
+		Debug.Log( resource + " " + range[0] + " " + range[1] );
+		List<int> waveList = new List<int>();
 
 		do {
 			int e = Random.Range( 1, range.Count );
 			if( range[ e ] > resource )
 			{
 				//Debug.Log( "loool " + range[ e ] + " ... " + resource );
-				wave.Add( resource );
+				waveList.Add( resource );
 				resource = 0;
 				break;
 			}
-			wave.Add( range[ e ] );
+			waveList.Add( range[ e ] );
 			resource -= range[ e ];
 			//Debug.Log( range[ e ] + " ... " + resource );
 		} while( resource > 0 );
 
-		Debug.Log( "wave count= " + wave.Count );
-		for (int i = 0; i < wave.Count; i++) {
-			Debug.Log( wave[ i ] );
+		//Debug.Log( "wave count= " + waveList.Count );
+		for ( int i = 0; i < waveList.Count; i++ ) {
+			//Debug.Log( waveList[ i ] );
 		}
-		return wave;
+		return waveList;
 	}
 
-	IEnumerator SpawnWave() 
+	IEnumerator SpawnWave( List<int> waveList )
 	{
-		if( this.wave < enemyWaves.Count -1 ) {
+		//if( this.wave < enemyWaves.Count -1 ) {
 			waveTime = 0f;
 			spawning = true; //TODO: Temp solution?
+		//Debug.Log(3);
+			//string wave = enemyWaves[ this.wave += 1 ];
 
-			string wave = enemyWaves[ this.wave += 1 ];
-
-			for( int i = 0; i < wave.Length / spawnPoints.Length; i++ ) 
+			for( int i = 0; i < waveList.Count / spawnPoints.Length; i++ ) 
 			{
 				yield return new WaitForSeconds( 1f ); // This breaks on level load for some reason
-				for (int j = 0; j < spawnPoints.Length; j++) 
+				for ( int j = 0; j < spawnPoints.Length; j++ ) 
 					enemies.Add( SpawnEnemy( j ) );
 			}
+			Debug.Log(3);
 			spawning = false;
-		}
+		//}
 	}
 
 	Enemy SpawnEnemy( int point ) 
